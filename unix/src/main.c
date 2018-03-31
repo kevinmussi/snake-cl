44460c0cce1f7d3bc1cd071503be7db7d5d8ba07
Kevin Mussi <kevinludovico.mussi@mail.polimi.it>
2018-03-31 22:14:32 +0200

    added argv evaluation

--- unix/src/main.c
+++ unix/src/main.c
@@ -1,15 +1,38 @@
 #include "snake.h"
 
 int main(int argc, char * argv[]) {
-    if(argc != 2) {
-        printf("Error! Launch with: ./snake <nickname>\n");
-        return 0;
+    char *par;
+    
+    // Default flags
+    boolean hard = False;
+    boolean no_obstacles = False;
+    boolean screen_wrap = False;
+    
+    // Evaluate the program parameters
+    int i;
+    for(i = 1; i < argc; i++) {
+        par = argv[i];
+        
+        if(!strcmp(par, "--hard")) {
+            hard = True;
+        } else if(!strcmp(par, "--screen-wrap") || !strcmp(par, "-sw")) {
+            screen_wrap = True;
+        } else if(!strcmp(par, "--no-obstacles") || !strcmp(par, "-no")) {
+            no_obstacles = True;
+        } else if(!strcmp(par, "--help") || !strcmp(par, "-h")) {
+            printf("Usage: snake [flags]\nFlags:\n\t");
+            printf("-h: print this help message.\n\t");
+            printf("-hard: play the hard mode.\n\t");
+            printf("-screen-wrap, -sw: play with the screen wrap mode.\n\t");
+            printf("-no-obstacles, -no: play without the generation of obstacles.\n\t");
+        } else {
+            printf("Error! Unrecognized flag %s!\n", par);
+            return -1;
+        }
     }
     
-    //nick = (char *) malloc(sizeof(char) * strlen(argv[1])); //alloca la memoria per il nickname
-    //strcpy(nick, argv[1]);
-    
-    launch(False, False, True);
+    // Launch the game
+    launch(no_obstacles, hard, screen_wrap);
     
     return 0;
 }
